a5060125077a8449de75aae1222ac064fda47ae7
Michael Louro <michael.aa.louro@gmail.com>
2021-01-21 23:51:26 -0500

    Add zsh plugins & submodules

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 "$SL" "$HERE_PROJECT/zshrc" "$OUT/.zshrc"
+"$SL" "$HERE_PROJECT/zsh-autosuggestions" "${ZSH_CUSTOM:-"$OUT/.oh-my-zsh/custom"}/plugins/zsh-autosuggestions"
+"$SL" "$HERE_PROJECT/zsh-syntax-highlighting" "${ZSH_CUSTOM:-"$OUT/.oh-my-zsh/custom"}/plugins/zsh-syntax-hightlighting"
