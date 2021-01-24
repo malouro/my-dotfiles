096316814f674afbcd19d8e652b20c70c19b2efa
Mike A. Louro <mike.a.louro@gmail.com>
2021-01-24 01:06:12 -0500

    Update zshrc setup
    Link plugins from git submodules, and source in zshrc

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-"$SL" "$HERE_PROJECT/zshrc" "$OUT/.zshrc"
-"$SL" "$HERE_PROJECT/zsh-autosuggestions" "${ZSH_CUSTOM:-"$OUT/.oh-my-zsh/custom"}/plugins/zsh-autosuggestions"
-"$SL" "$HERE_PROJECT/zsh-syntax-highlighting" "${ZSH_CUSTOM:-"$OUT/.oh-my-zsh/custom"}/plugins/zsh-syntax-hightlighting"
+"$SL" "$HERE_PROFILE" "$OUT/.zsh"
+"$SL" "$HERE_PROFILE/zshrc" "$OUT/.zshrc"
