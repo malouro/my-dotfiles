b6f6b63fa662f0b5a389c6cea7ee213b1dc71a02
Mike A. Louro <mike.a.louro@gmail.com>
2021-01-28 11:29:53 -0500

    bin scripts from Mac

---
+++ bin/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+"$SL" "$HERE_PROFILE/clean-npm" "$OUT/.bin/clean-npm"
+"$SL" "$HERE_PROFILE/clean-yarn" "$OUT/.bin/clean-yarn"
+"$SL" "$HERE_PROFILE/kill-port" "$OUT/.bin/kill-port"
+"$SL" "$HERE_PROFILE/no-mouse-accel" "$OUT/.bin/no-mouse-accel"
