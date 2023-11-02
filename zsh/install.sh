4f91edc449984c2c47ced4db9a68f4ae28351086
Mike A. Louro <mike.a.louro@gmail.com>
2023-11-02 11:18:51 -0400

    Add zsh-completions plugin

--- zsh/install.sh
+++ zsh/install.sh
@@ -5,6 +5,7 @@ OMZ="$OUT/.oh-my-zsh"
 
 "$SL" "$HERE_PROFILE/zshrc" "$OUT/.zshrc"
 "$SL" "$HERE_PROFILE/zsh-autosuggestions" "$OMZ/custom/plugins/zsh-autosuggestions"
+"$SL" "$HERE_PROFILE/zsh-completions" "$OMZ/custom/plugins/zsh-completions"
 "$SL" "$HERE_PROFILE/zsh-syntax-highlighting" "$OMZ/custom/plugins/zsh-syntax-highlighting"
 
 # .oh-my-zsh themes
