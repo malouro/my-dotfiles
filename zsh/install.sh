0a81b59cef15daeae24dc25180b321e9a2c40f55
Mike A. Louro <mike.a.louro@gmail.com>
2023-11-11 01:58:52 -0500

    Add oh-my-zsh themes & zshiggy theme

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,3 +7,13 @@ OMZ="$OUT/.oh-my-zsh"
 "$SL" "$HERE_PROFILE/zsh-autosuggestions" "$OMZ/custom/plugins/zsh-autosuggestions"
 "$SL" "$HERE_PROFILE/zsh-completions" "$OMZ/custom/plugins/zsh-completions"
 "$SL" "$HERE_PROFILE/zsh-syntax-highlighting" "$OMZ/custom/plugins/zsh-syntax-highlighting"
+
+# .oh-my-zsh themes
+for theme in "$HERE_PROFILE/themes/"*/; do
+	theme_name="$(basename $theme)"
+	theme_path="$theme$theme_name.zsh-theme"
+
+	if [ -f "$theme_path" ]; then
+		"$SL" "$theme_path" "$OMZ/themes/$theme_name.zsh-theme"
+	fi
+done
