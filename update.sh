599d51f3bd4e6c660e3dd935a019d3f4682d4faa
Mike A. Louro <mike.a.louro@gmail.com>
2023-11-12 15:37:08 -0500

    Run update.sh

--- update.sh
+++ update.sh
@@ -5,12 +5,10 @@ git submodule update --remote --merge
 
 cd "$HERE/node-scripts"
 
-if which node &> /dev/null; then
-if which npm &> /dev/null; then
+if [which node &> /dev/null] & [which npm &> /dev/null]; then
   npm install && npm run build
 else
   echo "Skipping node-scripts update; Node/npm not detected"
-fi
 fi
 
 cd $HERE
