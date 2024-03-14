e9158fc6fcbf4594f7ea0e3b15df913e1800fceb
Mike A. Louro <mike.a.louro@gmail.com>
2024-03-14 13:12:03 -0400

    [Fix] Check for icims folder

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 "$SL" "$HERE_PROFILE/alacritty.toml" "$OUT/.alacritty.toml"
+
+if [ -d "$OUT/.config" ]; then
+for file in "$HERE_PROFILE/themes/"*; do
+	filename=$(basename ${file});
+	"$SL" "$HERE_PROFILE/themes/$filename" "$OUT/.config/alacritty/$filename"
+done
+fi
