810590b84f6d5c2a007955dca566b5a4dd08107b
Mike A. Louro <mike.a.louro@gmail.com>
2024-05-03 14:08:25 -0400

    Fix Alacritty OS check in install script

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -5,7 +5,7 @@ USR_APPDATA="${USR_APPDATA:-/mnt/c/Users/micha/AppData/Roaming}"
 
 # Force this with 'OS=Cygwin'
 # Potentially, this could come in automatically from <root>/install.sh or somewhere.
-if [ $OS = "Cygwin" ]; then
+if [ "$OS" = "Cygwin" ]; then
 	# We copy because the symlink won't work across our systems the way we want here.
 	# So to sync up, run install.sh *again* after updating the alacritty.toml.
 	cp "$HERE_PROFILE/alacritty.toml" "$USR_APPDATA/alacritty/alacritty.toml"
